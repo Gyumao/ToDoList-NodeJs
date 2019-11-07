ae6c1633d79951ed3c43098f2f43e4eec53db90c
Richard Gobert <dev.richard.g@gmail.com>
2019-11-07 14:46:24 +0100

    Creation static file edit.js in public and addEventListener on  edit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({
     extended: false
 }));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     Item.findAll().then(items => {
@@ -60,6 +60,7 @@ app.get("/", (req, res) => {
                 .join("")}
          </ul>
         </div>
+        <script src="/js/edit.js"></script>
         </body>
         </html>`);
     });
---
+++ public/js/edit.js
@@ -0,0 +1,5 @@
+document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("edit-me")) {
+        alert("click Ok on Edit button");
+    }
+});
