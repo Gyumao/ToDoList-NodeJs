e4f357b86f2cddc62a8330ce893b148cb4d314f5
Richard Gobert <dev.richard.g@gmail.com>
2019-11-07 12:17:45 +0100

    modification post request to insert in db input content

--- app.js
+++ app.js
@@ -74,10 +74,15 @@ app.get("/", (req, res) => {
 
 
 app.post("/", (req, res) => {
-    Item.findAll().then(items => {
-        console.log("All items: ", JSON.stringify(items, null, 4));
-    });
-    res.send("ça marche, c'est tres bien")
+    Item.create({
+            item: req.body.item
+        })
+        .then(() => {
+            res.redirect("/")
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 app.listen(port);
